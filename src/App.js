0afd1a900f21f5f2e69b8cf3dc7dc998dfef0f5c
ChefDimas <dimalunik@aol.com>
2022-03-27 19:04:55 +0300

    MUI and FontAwesome added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Header from "./components/Header";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <WelcomePage />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ function Header() {
         <nav>
           {/* Left Part */}
           <div className="left-navbar">
-            <div className="navbar-item"></div>
-            <div className="navbar-item"></div>
-            <div className="navbar-item"></div>
+            <div className="navbar-item">Home</div>
+            <div className="navbar-item">Stock</div>
+            <div className="navbar-item">About It</div>
           </div>
           {/* IMG Logo */}
           <img src="" alt="" className="logo" />
           {/* Right Part */}
           <div className="right-navbar">
-            <div className="navbar-item"></div>
-            <div className="navbar-item"></div>
-            <div className="navbar-item"></div>
+            <div className="navbar-item">Reviews</div>
+            <div className="navbar-item">Payment and Delivery</div>
+            <div className="navbar-item">Contacts</div>
           </div>
         </nav>
       </div>
