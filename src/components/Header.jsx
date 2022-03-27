9204196eaf02b2ee3a54aac47e71e26aaf37f996
ChefDimas <dimalunik@aol.com>
2022-03-27 21:31:12 +0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../style/header.sass";
+import logo from "../img/logo.png";
 
 function Header() {
   return (
     <div className="container">
-      <div className="navbar">
-        <nav>
+      <div>
+        <nav className="navbar">
           {/* Left Part */}
           <div className="left-navbar">
             <div className="navbar-item">Home</div>
@@ -14,8 +14,14 @@ function Header() {
             <div className="navbar-item">About It</div>
           </div>
           {/*Logo */}
-          <div>
-            <FontAwesomeIcon icon="fa-solid fa-seedling"></FontAwesomeIcon>
+          <div className="logo-container">
+            <img
+              src={logo}
+              alt="logo-img"
+              className="logo"
+              height={80}
+              width={100}
+            />
           </div>
           {/* Right Part */}
           <div className="right-navbar">
