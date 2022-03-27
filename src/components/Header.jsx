a9471a2e2ec33f659d9c262779bd02e82c55b62b
ChefDimas <dimalunik@aol.com>
2022-03-27 18:46:01 +0300

    Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "../style/header.sass";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="container">
+      <div className="navbar">
+        <nav>
+          {/* Left Part */}
+          <div className="left-navbar">
+            <div className="navbar-item"></div>
+            <div className="navbar-item"></div>
+            <div className="navbar-item"></div>
+          </div>
+          {/* IMG Logo */}
+          <img src="" alt="" className="logo" />
+          {/* Right Part */}
+          <div className="right-navbar">
+            <div className="navbar-item"></div>
+            <div className="navbar-item"></div>
+            <div className="navbar-item"></div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
