fb204616ce91597e5a87f4460f9e6698391afbe5
ChefDimas <dimalunik@aol.com>
2022-04-07 10:39:41 +0300

    reset nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,33 +4,31 @@ import logo from "../img/logo.png";
 
 function Header() {
   return (
-    <div className="container">
-      <div>
-        <nav className="navbar">
-          {/* Left Part */}
-          <div className="left-navbar">
-            <div className="navbar-item">Home</div>
-            <div className="navbar-item">Stock</div>
-            <div className="navbar-item">About It</div>
-          </div>
-          {/*Logo */}
-          <div className="logo-container">
-            <img
-              src={logo}
-              alt="logo-img"
-              className="logo"
-              height={80}
-              width={100}
-            />
-          </div>
-          {/* Right Part */}
-          <div className="right-navbar">
-            <div className="navbar-item">Reviews</div>
-            <div className="navbar-item">Payment and Delivery</div>
-            <div className="navbar-item">Contacts</div>
-          </div>
-        </nav>
-      </div>
+    <div className="nav-container">
+      <nav>
+        {/* Left Part */}
+        <div className="left-navbar">
+          <div className="navbar-item">Home</div>
+          <div className="navbar-item">Stock</div>
+          <div className="navbar-item">About It</div>
+        </div>
+        {/*Logo */}
+        <div className="logo-container">
+          <img
+            src={logo}
+            alt="logo-img"
+            className="logo"
+            height={80}
+            width={100}
+          />
+        </div>
+        {/* Right Part */}
+        <div className="right-navbar">
+          <div className="navbar-item">Reviews</div>
+          <div className="navbar-item">Payment and Delivery</div>
+          <div className="navbar-item">Contacts</div>
+        </div>
+      </nav>
     </div>
   );
 }
