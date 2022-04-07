5434a2758f8685e245df80a69d3e4af9a3af89e4
ChefDimas <dimalunik@aol.com>
2022-04-07 11:14:00 +0300

    finished nav

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "../components/Header";
+import welcomepage from "../style/welcomepage.sass";
 
 function WelcomePage() {
   return (
-    <div>
+    <div className="page">
       <Header />
     </div>
   );
